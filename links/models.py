610ed802b513d12a07fa127e128b4a031e437dc9
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-08-21 12:14:01 -0300

    Create models and make migration

--- links/models.py
+++ links/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Recommendation(models.Model):
+    address = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, blank=True)
+    tag = models.CharField(max_length=35, blank=True)
+    recommender = models.CharField(max_length=100, blank=True)
+    recorder = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        abstract = True
+
+class Article(Recommendation):
+    pass
+
+class Video(Recommendation):
+    pass
+
+class Podcast(Recommendation):
+    pass
