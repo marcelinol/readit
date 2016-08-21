00f679fa1d719cf3e8f690bcf8e198c87479ec8e
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-08-21 12:59:08 -0300

    Create the first view and load all articles

--- links/views.py
+++ links/views.py
@@ -1,5 +1,13 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the links index.")
+    articles_list = Article.objects.all()
+    template = loader.get_template('links/index.html')
+    context= {
+        'articles_list': articles_list,
+    }
+    return HttpResponse(template.render(context, request))
