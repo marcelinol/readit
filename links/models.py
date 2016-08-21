dddde393ee79480e2ab368b5fbe9ca25c9bcbf22
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-08-21 12:58:59 -0300

    add created_at field

--- links/models.py
+++ links/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 class Recommendation(models.Model):
-    address = models.CharField(max_length=200)
-    title = models.CharField(max_length=200, blank=True)
-    tag = models.CharField(max_length=35, blank=True)
+    address     = models.CharField(max_length=200)
+    title       = models.CharField(max_length=200, blank=True)
+    tag         = models.CharField(max_length=35, blank=True)
     recommender = models.CharField(max_length=100, blank=True)
-    recorder = models.CharField(max_length=100, blank=True)
+    recorder    = models.CharField(max_length=100, blank=True)
+    created_at  = models.DateTimeField(editable=False, default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created_at = timezone.now()
+        return super(Recommendation, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         abstract = True
